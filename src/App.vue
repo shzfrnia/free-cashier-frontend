01be2e2354cf56a910e33857734aa591304f5aac
Unknown <pugumo68@gmail.com>
2019-07-01 15:33:08 +0700

    Init

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div :style="{'background-color': selectedBgColor}"  id="app">
+      <nav-bar></nav-bar>
+      <router-view/>
+  </div>
+</template>
+
+
+<script>
+  import NavBar from './components/NavBar'
+
+  export default {
+    name: "app",
+    components: {NavBar},
+    computed: {
+      selectedBgColor() {
+        if (this.$route.path === '/home') return "#3EAF6F"
+        return this.$route.meta.bkColor
+      }
+    }
+  }
+</script>
+
+
+<style>
+    @import 'assets/fonts/font-awesome.css';
+
+    * {
+        margin: 0px;
+        padding: 0px;
+    }
+
+    #app {
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        height: 100vh;
+    }
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,62 @@
+<template>
+    <div id="nav">
+        <div class="my-btn-container">
+            <router-link :key="route.icon" :to="route.route" v-for="route in routes">
+                <div class="my-btn" :key="route.icon">
+                    <i :style="{color: route.bkColor}" :class="['fas', `fa-${route.icon}`]"></i>
+                </div>
+            </router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "NavBar",
+    data() {
+      return {
+        routes: [
+          {
+            icon: 'user-circle',
+            route: '/',
+            bkColor: '#3EAF6F'
+          },
+          {
+            icon: 'user',
+            route: '/about',
+            bkColor : '#BE0031'
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    .my-btn-container {
+        display: flex;
+        /*background-color: black;*/
+        /*padding-top: 150px;*/
+        /*padding-bottom: 150px;*/
+        justify-content: center;
+    }
+
+    .my-btn {
+        display: inline-block;
+        cursor: pointer;
+        width: 50px;
+        height: 50px;
+        margin: 5px;
+        font-size: 25px;
+        color: gray;
+        border-radius: 25px;
+        background-color: white;
+        text-align: center;
+        line-height: 50px;
+        -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+        transition: 0.5s;
+    }
+    .router-link-active .my-btn{
+        width: 80px;
+    }
+</style>
