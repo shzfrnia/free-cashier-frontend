920b0f7ab604509c949d5d515015fe4598be7733
Keparisss <44916387+Keparisss@users.noreply.github.com>
2019-07-02 13:30:21 +0700

    Добавлен логотип

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,12 +1,26 @@
 <template>
-    <div>
-        <img style="width: 100%;top: 160px;position: relative;" src="../../public/ы.png" alt="S">
-    </div>
+  <div class="about">
+    <img src="../../public/ы.jpg"/>
+    <hr/>
+    <h1>Контактная информация</h1>
+    <div class="text">Наш адрес : Ленина 87</div>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2203.361976972366!2d84.94812231577775!3d56.478751943235395!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4326935f07895e49%3A0x282cd6bec28b6188!2z0L_RgC4g0JvQtdC90LjQvdCwLCA4Nywg0KLQvtC80YHQuiwg0KLQvtC80YHQutCw0Y8g0L7QsdC7LiwgNjM0MDUw!5e0!3m2!1sru!2sru!4v1561992377840!5m2!1sru!2sru" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+    <div class="text">Контактный номер : 506078</div>
+  </div>
 </template>
 
-<script>
-
-  export default {
-    name: 'about'
-  }
-</script>
+<style scope>
+img{height: 300px;}
+.about{background-color:white;height: 100%;}
+.text {
+  font-size: 40px;
+  margin-top: 1%;
+}
+h1{margin-top:1%;}
+hr {
+border: none; /* Убираем границу */
+background-color:#BE0031; /* Цвет линии */
+color:#BE0031; /* Цвет линии для IE6-7 */
+height: 8px; /* Толщина линии */
+}
+</style>
