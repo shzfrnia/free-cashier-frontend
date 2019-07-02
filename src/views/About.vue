77ec69b6223cefb0e56ae48322505243f892aeeb
Unknown <pugumo68@gmail.com>
2019-07-02 16:05:37 +0700

    Init Contact info

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,45 +1,39 @@
 <template>
   <div class="about">
-<!--    <img src="../../public/ы.jpg"/>-->
-    <hr/>
-<!--    <h1>Контактная информация</h1>-->
       <button
               v-for="tab in tabs"
               v-bind:key="tab"
               v-on:click="currentTab = tab.componentName"
-              v-bind:class="[{ active: currentTab === tab.componentName }]"
-      >
-                {{ tab.visibleName }}
+              v-bind:class="[{ active: currentTab === tab.componentName }]">
+        {{ tab.visibleName }}
       </button>
       <keep-alive>
         <component v-bind:is="currentTab"></component>
       </keep-alive>
-<!--    <div class="text">Наш адрес : Ленина 87</div>-->
-<!--    <div class="text">Контактный номер : 506078</div>-->
   </div>
 </template>
 
 <script>
     import GoogleMap from '../components/GoogleMap'
-    import WhiteBox from '../components/WhiteBox'
+    import ContactInfo from '../components/ContactInfo'
 
   export default {
     name: "about",
     components: {
       GoogleMap,
-      WhiteBox
+      ContactInfo
     },
     data() {
       return {
-        currentTab : 'google-map',
+        currentTab : 'contact-info',
         tabs : [
+          {
+            componentName: 'contact-info',
+            visibleName: "Contacts"
+          },
           {
             componentName: 'google-map',
             visibleName: 'MAP'
-          },
-          {
-            componentName: 'white-box',
-            visibleName: "BOX"
           }
         ]
       }
@@ -48,22 +42,7 @@
 </script>
 
 <style scoped>
-    img{
-        height: 300px;
-    }
-    .about{
-        background-color:white;height: 100%;
-    }
     .active {
       background: red;
-    }
-    h1{
-        margin-top:1%;
-    }
-    hr {
-        border: none; /* Убираем границу */
-        background-color:#BE0031; /* Цвет линии */
-        color:#BE0031; /* Цвет линии для IE6-7 */
-        height: 8px; /* Толщина линии */
     }
 </style>
