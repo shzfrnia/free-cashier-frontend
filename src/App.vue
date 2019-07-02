54f98ab8c8dc449dcd7a4850568f50c4d41a91d7
Unknown <pugumo68@gmail.com>
2019-07-02 20:56:22 +0700

    Added animation

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div :style="{'background-image': `url(${selectedBgColor})`}"  id="app">
-      <modal-window @submit="modal=false" v-if="modal"></modal-window>
+      <transition name="bounce">
+        <modal-window @submit="modal=false" v-if="modal"></modal-window>
+      </transition>
       <nav-bar></nav-bar>
       <white-box>
           <transition name="component-fade" mode="out-in">
@@ -38,6 +40,28 @@
     }
   }
 </script>
+
+<style scoped>
+    .bounce-enter-active {
+        animation: bounce-in .5s;
+    }
+    .bounce-leave-active {
+        animation: bounce-in .5s reverse;
+    }
+    @keyframes bounce-in {
+        0% {
+            transform: scale(0);
+        }
+        50% {
+            transform: scale(1.5);
+        }
+        100% {
+            transform: scale(1);
+        }
+    }
+
+</style>
+
 
 
 <style>
