a54f79dd180eac393df44d3bdb7bed63a80f1d5b
Keparisss <44916387+Keparisss@users.noreply.github.com>
2019-07-02 19:10:02 +0700

    form added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,23 @@
 <template>
   <div>
-      <img width="100%" height="auto" src="../../public/СПАНЧ.png">
+      <img width="10%" height="auto" src="../../public/СПАНЧ.png">
+      <form action="post">
+       <div class="text "> Имя <input type="text" name="Name" class="text"></div><br/>
+       <div class="text ">Контактный номер <input type="tel" name="Phone" class="text" maxlength="11"></div><br/>
+        <!-- Количество человек <select name="People_count" class="text"><br/>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+        </select><br/> -->
+        <div class="text ">Время брони <input type="time" name="Booking_time" class="text"></div><br/>
+        <div class="text red">Стол свободен до @TIME-30мин</div><br/>
+        <input type="submit" value="Забронировать" name="submit" id="submit" class="text">
+      </form>
   </div>
 </template>
 
@@ -9,4 +26,19 @@
 export default {
   name: 'home'
 }
+
+// $(function(){
+//   //2. Получить элемент, к которому необходимо добавить маску
+//   $("#Phone").mask("8(999) 999-9999");
+// });
+
 </script>
+<style scoped>
+  #submit{
+    background-color: #CC8602;
+    border: none;
+    color: white;}
+  .text{margin-top: 1%;font-size: 30px;}
+  .red{color: red;}
+
+</style>
