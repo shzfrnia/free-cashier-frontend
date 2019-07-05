6e37acae8e55366fed073102b2c83ce3260a6ebe
Unknown <pugumo68@gmail.com>
2019-07-06 00:29:18 +0700

    Стильно, модно, молодежно

--- src/views/AdminPanel.vue
+++ src/views/AdminPanel.vue
@@ -1,38 +1,12 @@
 <template>
     <div class="container">
-        <component :is="currentTab"></component>
+        <router-view></router-view>
     </div>
 </template>
 
 <script>
-    import AdminLogin from '../components/Admin/AdminLogin'
-    import AdminMenu from '../components/Admin/AdminMenu'
-
   export default {
     name: "AdminPanel",
-    components: {
-      AdminLogin,
-      AdminMenu
-    },
-    data() {
-      return {
-        tabs: [
-          {
-            componentName: 'admin-login',
-            displayName: 'admin login'
-          },
-          {
-            componentName: 'admin-menu',
-            displayName: 'Admin Menu'
-          }
-        ]
-      }
-    },
-    computed: {
-      currentTab() {
-        return this.$store.getters.isAuthenticated ? this.tabs[1].componentName : this.tabs[0].componentName
-      }
-    }
   }
 </script>
 
