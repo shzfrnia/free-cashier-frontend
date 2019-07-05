d5004087e76007a1425e972c4460d8b65a3e4553
Keparisss <44916387+Keparisss@users.noreply.github.com>
2019-07-05 15:39:33 +0700

    added data for Reservation

--- src/api/mock/data.js
+++ src/api/mock/data.js
@@ -119,3 +119,80 @@ export const FakeTables = {
         }
     ]
 }
+
+export const FakeReservation = {
+    '0':[
+        {
+            id: 0,
+            name: "Марфа",
+            time: "4368431800734107",
+            Id: 101,
+            idTable: 107,
+        },
+        {
+            id: 1,
+            name: "Алина",
+            time: "4368431800734107",
+            Id: 101,
+            idTable: 102,
+        },
+        {
+            id: 2,
+            name: "Сергей",
+            time: "74030804388038",
+            Id: 101,
+            idTable: 103,
+        },
+        {
+            id: 3,
+            name: "Павел",
+            time: "7347830808730785",
+            Id: 101,
+            idTable: 104,
+        },
+        {
+            id: 4,
+            name: "Марина",
+            time: "57889458458",
+            Id: 101,
+            idTable: 105,
+        },
+    ],
+    '1':[
+        {
+            id: 5,
+            name: "Марина",
+            time: "57889458458",
+            Id: 101,
+            idTable: 102,
+        },
+        {
+            id: 6,
+            name: "Сабина",
+            time: "8385395329538",
+            Id: 101,
+            idTable: 102,
+        },
+        {
+            id: 7,
+            name: "Марк",
+            time: "57889458458",
+            Id: 101,
+            idTable: 103,
+        },
+        {
+            id: 8,
+            name: "Дуня",
+            time: "58932595327",
+            Id: 101,
+            idTable: 104,
+        },
+        {
+            id: 9,
+            name: "Петр",
+            time: "4826699634",
+            Id: 101,
+            idTable: 101,
+        },
+    ],
+}
